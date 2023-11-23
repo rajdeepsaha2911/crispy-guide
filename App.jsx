05dd768ed09d883c38b2cf467800691a71895ac6
rajdeepsaha2911 <137227037+rajdeepsaha2911@users.noreply.github.com>
2023-11-23 21:51:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Testimonial from "./pages/Testimonial";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Testimonial/>
+      </div>
+    </>
+  );
+}
+export default App;
